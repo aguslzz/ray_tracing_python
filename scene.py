f7843548a061bb9383e24125a35531cc067f6449
Agustín López Zapata <aguslopezzapata10@gmail.com>
2023-02-12 22:59:07 -0500

    creating scenes in 2D with 3D objects
    first test has failed, some traceback errors have been shown.

---
+++ scene.py
@@ -0,0 +1,7 @@
+class Scene:
+    """Scene has only information needed for the ray tracing engine"""
+    def __init__(self, camera, objects, width, height):
+        self.camera = camera
+        self.objects = objects
+        self.width = width
+        self.height = height
